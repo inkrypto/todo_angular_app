4a1760e16ccb84395c96ec8b80a9498f64b0c31f
brent morris <inkrypto@gmail.com>
2015-03-30 20:31:24 -0500

    strikethrough

--- app/assets/javascripts/task_ctrl.js
+++ app/assets/javascripts/task_ctrl.js
@@ -3,15 +3,25 @@
 
   angular.module("app").controller("taskCtrl", function($scope) {
 
-    $scope.tasks = ["one", "two", "three"];
+    $scope.tasks = [];
 
     $scope.addTask = function(task) {
       if(task && $scope.tasks.indexOf(task) === -1) {
-        $scope.task.push(task);
-        $scope.newTask = "";
+        $scope.tasks.push({task: task, completed: false});
+        $scope.newTask = undefined;
       } 
     }
    
+    $scope.toggleComplete = function(task){
+      task.completed = !task.completed;
+    }
+
+    $scope.deleteTask = function(task){
+      if(task && $scope.task.indexOf(task));
+      $scope.tasks.pop(task);
+
+    }
+
     window.scope = $scope;      
 
   });
